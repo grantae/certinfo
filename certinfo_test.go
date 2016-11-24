f23da35a0740d0b8340ee71e537e45e730bb7eaf
Deomid Ryabkov <rojer@cesanta.com>
2016-11-24 18:45:13 +0000

    Add CertificateRequestText to handle CSRs

--- certinfo_test.go
+++ certinfo_test.go
@@ -5,11 +5,19 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
+type InputType int
+
+const (
+	tCertificate InputType = iota
+	tCertificateRequest
+)
+
 // Compares a PEM-encoded certificate to a refernce file.
-func testPair(t *testing.T, certFile, refFile string) {
+func testPair(t *testing.T, certFile, refFile string, inputType InputType) {
 	// Read and parse the certificate
 	pemData, err := ioutil.ReadFile(certFile)
 	if err != nil {
@@ -19,13 +27,26 @@ func testPair(t *testing.T, certFile, refFile string) {
 	if block == nil || len(rest) > 0 {
 		t.Fatal("Certificate decoding error")
 	}
-	cert, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		t.Fatal(err)
-	}
-	result, err := CertificateText(cert)
-	if err != nil {
-		t.Fatal(err)
+	var result string
+	switch inputType {
+	case tCertificate:
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			t.Fatal(err)
+		}
+		result, err = CertificateText(cert)
+		if err != nil {
+			t.Fatal(err)
+		}
+	case tCertificateRequest:
+		cert, err := x509.ParseCertificateRequest(block.Bytes)
+		if err != nil {
+			t.Fatal(err)
+		}
+		result, err = CertificateRequestText(cert)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 	resultData := []byte(result)
 
@@ -35,27 +56,32 @@ func testPair(t *testing.T, certFile, refFile string) {
 		t.Fatal(err)
 	}
 	if !bytes.Equal(resultData, refData) {
-		t.Logf("Certificate '%s' did not match reference '%s'\n", certFile, refFile)
-		t.Errorf("Certificate dump follows:\n%s\n", result)
+		t.Logf("'%s' did not match reference '%s'\n", certFile, refFile)
+		t.Errorf("Dump follows:\n%s\n", result)
+		os.Stderr.WriteString(result)
 	}
 }
 
 // Test the root CA certificate
 func TestCertInfoRoot(t *testing.T) {
-	testPair(t, "test_certs/root1.cert.pem", "test_certs/root1.cert.text")
+	testPair(t, "test_certs/root1.cert.pem", "test_certs/root1.cert.text", tCertificate)
+	testPair(t, "test_certs/root1.csr.pem", "test_certs/root1.csr.text", tCertificateRequest)
 }
 
 // Test the leaf (user) RSA certificate
 func TestCertInfoLeaf1(t *testing.T) {
-	testPair(t, "test_certs/leaf1.cert.pem", "test_certs/leaf1.cert.text")
+	testPair(t, "test_certs/leaf1.cert.pem", "test_certs/leaf1.cert.text", tCertificate)
+	testPair(t, "test_certs/leaf1.csr.pem", "test_certs/leaf1.csr.text", tCertificateRequest)
 }
 
 // Test the leaf (user) DSA certificate
 func TestCertInfoLeaf2(t *testing.T) {
-	testPair(t, "test_certs/leaf2.cert.pem", "test_certs/leaf2.cert.text")
+	testPair(t, "test_certs/leaf2.cert.pem", "test_certs/leaf2.cert.text", tCertificate)
+	testPair(t, "test_certs/leaf2.csr.pem", "test_certs/leaf2.csr.text", tCertificateRequest)
 }
 
 // Test the leaf (user) ECDSA certificate
 func TestCertInfoLeaf3(t *testing.T) {
-	testPair(t, "test_certs/leaf3.cert.pem", "test_certs/leaf3.cert.text")
+	testPair(t, "test_certs/leaf3.cert.pem", "test_certs/leaf3.cert.text", tCertificate)
+	testPair(t, "test_certs/leaf3.csr.pem", "test_certs/leaf3.csr.text", tCertificateRequest)
 }
